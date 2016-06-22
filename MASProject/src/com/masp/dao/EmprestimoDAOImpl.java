001143fa7ea9d1278436e0c7994d7d6c4dc44949
vitor.arantes <vitor.arantes@ncei-4535.centropaulasouza.sp.gov.br>
2016-06-22 17:52:03 -0300

    Implementado alguns metodos

--- MASProject/src/com/masp/dao/EmprestimoDAOImpl.java
+++ MASProject/src/com/masp/dao/EmprestimoDAOImpl.java
@@ -1,26 +1,70 @@
 package com.masp.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.masp.entity.Artista;
 import com.masp.entity.Emprestimo;
 
 public class EmprestimoDAOImpl implements EmprestimoDAO{
 
 	@Override
 	public void adicionar(Emprestimo e) {
-		// TODO Auto-generated method stub
-		
+		Connection con = DBUtil.getInstancia().openConnection();
+		String sql = "INSERT INTO emprestimo (id, id_obra, destino, " +
+				 	"dataInicial, dateFinal, museu, custo) " +
+					" VALUES (?, ?, ?, ?, ?, ?, ?) ";
+		try {
+			PreparedStatement st = con.prepareStatement( sql );
+			st.setLong( 1, e.getId() );
+			st.setLong( 2, e.getObra().getId() );
+			st.setString( 3, e.getDestino() );
+			java.sql.Date d = new java.sql.Date ( e.getDataInicial().getTime() );
+			st.setDate(4, d );
+			d = new java.sql.Date ( e.getDataFinal().getTime() );
+			st.setDate(5, d );
+			st.setString( 6,  e.getMuseu() );
+			st.setFloat( 7, e.getCusto() );
+			st.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		DBUtil.getInstancia().closeConnection();
 	}
 
 	@Override
 	public List<Emprestimo> pesquisar(String museu) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Emprestimo> lista = new ArrayList<Emprestimo>();
+		Emprestimo a = new Emprestimo();
+		Connection con = DBUtil.getInstancia().openConnection();
+		String sql = "SELECT * FROM artista WHERE nome like ? ";
+		try {
+			PreparedStatement st = con.prepareStatement( sql );
+			st.setString( 1, "%" + museu + "%" );
+			ResultSet rs = st.executeQuery();
+			while( rs.next() ) {
+				a.setId(  rs.getLong("id")  );
+//				a.setObra( (  rs.getLong("id_obra")  );
+				a.setDestino(  rs.getString("destino")  );
+				a.setDataInicial( rs.getDate("dataInicial") );
+				a.setDataFinal(  rs.getDate("dataFinal") );
+				a.setMuseu( rs.getString("museu") );
+				a.setCusto( rs.getFloat("custo") );
+				lista.add( a );
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		DBUtil.getInstancia().closeConnection();	
+		return lista;
 	}
 
 	@Override
 	public void atualizar(Emprestimo oldM, Emprestimo newM) {
-		// TODO Auto-generated method stub
 		
 	}
 	
--- MASProject/src/com/masp/dao/ExposicaoDAOImpl.java
+++ MASProject/src/com/masp/dao/ExposicaoDAOImpl.java
@@ -1,5 +1,91 @@
 package com.masp.dao;
 
-public class ExposicaoDAOImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.masp.entity.Exposicao;
+
+public class ExposicaoDAOImpl implements ExposicaoDAO{
+
+	@Override
+	public void adicionar(Exposicao ex) {
+		Connection con = DBUtil.getInstancia().openConnection();
+		String sql = "INSERT INTO exposicao (id, titulo, dtInicio, dtFim, tema, descricao) " +
+					 " VALUES (?, ?, ?, ?, ?, ?) ";
+		try {
+			PreparedStatement st = con.prepareStatement( sql );
+			st.setLong( 1, ex.getId() );
+			st.setString(2, ex.getTitulo() );
+			java.sql.Date d = new java.sql.Date( ex.getDtInicio().getTime() );
+			st.setDate( 3, d );
+			d = new java.sql.Date( ex.getDtFim().getTime() );
+			st.setDate( 4, d );
+			st.setString( 5,  ex.getTema() );
+			st.setString( 6, ex.getDescricao() );
+			st.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		DBUtil.getInstancia().closeConnection();
+	}
+
+	@Override
+	public Exposicao pesquisar(String nome) {
+		Exposicao ing = new Exposicao();
+		Connection con = DBUtil.getInstancia().openConnection();
+		String sql = "SELECT * FROM exposicao WHERE nome like ? ";
+		try {
+			PreparedStatement st = con.prepareStatement( sql );
+			st.setString( 1, "%" + nome + "%" );
+			ResultSet rs = st.executeQuery();
+			ing.setId(  rs.getLong("id")  );
+			ing.setTitulo(  rs.getString("titulo")  );
+			ing.setDtInicio( rs.getDate("dtInicio") );
+			ing.setDtFim( rs.getDate("dtFim") );
+			ing.setDescricao( rs.getString("descricao") );
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		DBUtil.getInstancia().closeConnection();	
+		return ing;
+	}
+
+	@Override
+	public void remover(String nome) {
+		Connection con = DBUtil.getInstancia().openConnection();
+		String sql = "DELETE FROM exposicao WHERE nome = ?";
+		try {
+			PreparedStatement st = con.prepareStatement( sql );
+			st.setString( 1, nome );
+			st.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		DBUtil.getInstancia().closeConnection();
+	}
+
+	@Override
+	public void atualizar(Exposicao oldE, Exposicao newE) {
+		Connection con = DBUtil.getInstancia().openConnection();
+		String sql = "UPDATE categoria SET (?, ?, ?, ?, ?, ?) WHERE " +
+					 "nome = ?";
+		try {
+			PreparedStatement st = con.prepareStatement( sql );
+			st.setLong( 1, newE.getId() );
+			st.setString( 2, newE.getTitulo() );
+			java.sql.Date d = new java.sql.Date( newE.getDtInicio().getTime() );
+			st.setDate( 3, d );
+			d = new java.sql.Date( newE.getDtFim().getTime() );
+			st.setDate( 4, d );
+			st.setString( 5, newE.getDescricao() );
+			st.setString( 6, oldE.getTitulo() );
+			st.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		DBUtil.getInstancia().closeConnection();
+	}
 
 }
